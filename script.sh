63bb0322c16a1b498c3fbf867e5cde4c6398c12e
vitao2026 <vitordemota@gmail.com>
2022-12-22 13:15:25 -0400

    projeto deploy

---
+++ script.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echon "Criando as imagens......."
+
+docker build -t vitao2026/projeto-backend:1.0 backend/. 
+docker build -t vitao2026/projeto-database:1.0 database/.
+
+echo "Realizando o push das imagens..."
+
+docker push vitao2026/projeto-backend:1.0 
+docker push vitao2026/projeto-database:1.0
+
+echo "Criando serviços no cluster.."
+
+kubectl apply -f ./services.yml
+
+echo "Criando os deployments.."
+
+kubectl apply -f ./deployment.yml
